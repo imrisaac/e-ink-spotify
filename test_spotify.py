d0fde53345a57dc2299a91c2d3b616a61237c610
imrisaac <imrisaac@gmail.com>
2023-12-19 11:47:41 -0500

    add batery icon display

--- test_spotify.py
+++ test_spotify.py
@@ -1,22 +1,19 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from PIL import Image
+from PIL import Image, ImageStat
 import requests
-import smbus
-from inky.auto import auto
 
 SPOTIPY_CLIENT_ID = 'd3ee96d71f0545909f0529a7e8788bef'
 SPOTIPY_CLIENT_SECRET = '182479cb240640638cb0a28a8580f4c4'
 SPOTIPY_REDIRECT_URI = 'http://localhost:8000/callback'
 
-WITTY_PI_DEVICE_ADDRESS = 0x08
-
-bus = smbus.SMBus(1)
-
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(SPOTIPY_CLIENT_ID, 
-                    SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope='user-top-read', open_browser=False, cache_path='/home/pi/spotipy/.cache'))
+                    SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope='user-top-read', open_browser=False, cache_path='.cache'))
 
-# resize_image and retaion aspect ratio
+top_tracks = sp.current_user_top_tracks(time_range='short_term', limit=6)
+
+images = []
+
 def resize_image(input_path, base_width=None, base_height=None):
     img = Image.open(input_path)
 
@@ -37,36 +34,8 @@ def resize_image(input_path, base_width=None, base_height=None):
 
     return img
 
-# Function to read the voltage from the I2C device
-def read_voltage():
-    try:
-        # Read the integer part (first register)
-        integer_part = bus.read_byte_data(WITTY_PI_DEVICE_ADDRESS, 0x17)
-
-        # Read the decimal part (second register)
-        decimal_part = bus.read_byte_data(
-            WITTY_PI_DEVICE_ADDRESS, 0x18) / 100.0  # Assuming two decimal places
-
-        # Combine integer and decimal parts to get the voltage
-        voltage = integer_part + decimal_part
-        return voltage
-    except Exception as e:
-        print(f"Error reading voltage: {str(e)}")
-        return None
-
 def is_batt_low():
     return True
-
-# Read the voltage from the I2C device
-voltage = read_voltage()
-if voltage is not None:
-    print(f"Voltage: {voltage:.2f} V")
-else:
-    print("Failed to read voltage.")
-
-top_tracks = sp.current_user_top_tracks(time_range='short_term', limit=6)
-
-images = []
 
 for track in top_tracks['items']:
     album_art_url = track['album']['images'][0]['url']
@@ -95,11 +64,4 @@ if is_batt_low():
 
     combined_image.paste(battery_image, position, battery_image)
 
-# combined_image.show();
-
-inky = auto(ask_user=True, verbose=True)
-saturation = 1.0
-
-resizedimage = combined_image.resize(inky.resolution).rotate(180)
-
-inky.set_image(resizedimage, saturation=saturation)
+combined_image.show()
